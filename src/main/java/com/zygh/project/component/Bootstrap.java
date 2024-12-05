c71abb2b7de76573a74c54e4d19587533a6d1cc7
mac <791199541@qq.com>
2024-12-05 15:43:31 +0800

    1.0

---
+++ src/main/java/com/zygh/project/component/Bootstrap.java
@@ -0,0 +1,15 @@
+package com.zygh.project.component;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ZhuHongYu on 2024/12/3.
+ */
+@Component
+public class Bootstrap implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
